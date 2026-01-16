e2499a95107bf27f20b96ed9a49491b3ec7d889c
Karen Johnstron <228061156+KE-Johnston1@users.noreply.github.com>
2026-01-16 15:19:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+document.getElementById("qr-input").addEventListener("change", () => {
+    document.getElementById("decoded-text").textContent = "Decoding...";
+    document.getElementById("risk-output").textContent = "Analysing...";
+    document.getElementById("result").classList.remove("hidden");
+});
 
